4a77abebc8b3483215dd96613a3be4a3d05d1d28
Saymon <mdsymon703@gmail.com>
2023-11-10 20:09:11 +0600

    init

---
+++ resources/views/user.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Assignment7</title>
+
+    </head>
+    <body class="antialiased">
+           <h1>Hello laravel from view</h1>
+    </body>
+</html>
